dcd23c51305b0622c89b9d55772b1f21cc807936
Tony Vu <vmthanh.bi@gmail.com>
2021-03-04 16:25:09 +0100

    Initial Commit

---
+++ Shared/WasteWatchersApp.swift
@@ -0,0 +1,17 @@
+//
+//  WasteWatchersApp.swift
+//  Shared
+//
+//  Created by Tony Vu on 3/4/21.
+//
+
+import SwiftUI
+
+@main
+struct WasteWatchersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
